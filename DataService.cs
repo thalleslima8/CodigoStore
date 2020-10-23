5cf9b1d177a2ccceeaa7f43bf89e67723d30469d
Thalles Veloso <thalleslima8@gmail.com>
2020-10-22 22:06:51 -0300

    Seeded DB Produtos table

--- DataService.cs
+++ DataService.cs
@@ -1,4 +1,8 @@
-﻿using CodigoStore.Context;
+﻿using CasaDoCodigo.Models;
+using CodigoStore.Context;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CodigoStore
 {
@@ -14,7 +18,27 @@ namespace CodigoStore
         public void InicializaDB()
         {
             contexto.Database.EnsureCreated();
+
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+
+            foreach (var livro in livros)
+            {
+                contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+            }
+
+            contexto.SaveChanges();
+
         }
     }
+
+    class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+     
 
 }
